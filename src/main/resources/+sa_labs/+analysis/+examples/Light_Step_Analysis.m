a4dd463fea73fb5cc1485becc8baa45f0222e3ce
Sathish Kumar Narayanan <ragav.sathish@gmail.com>
2017-05-10 11:45:29 +0300

    specify multiple amplifier streams in preset

--- src/main/resources/+sa_labs/+analysis/+examples/Light_Step_Analysis.m
+++ src/main/resources/+sa_labs/+analysis/+examples/Light_Step_Analysis.m
@@ -1,10 +1,28 @@
 %%
 tbUseProject('sa-labs-analysis', 'online', false);
-%%
+
+%% Create Analysis Project
+
 clear;
 project = createAnalysisProject('Example-LightStep-Analysis', 'experiments', '20170407') %#ok
 
-%%
+%% Create Analysis structure
+analysisPreset = struct();
+analysisPreset.type = 'analysis-type-1';
+analysisPreset.buildTreeBy = {'displayName', 'chan1Mode, chan2Mode, chan3Mode, chan4Mode'};
+analysisPreset.chan1Mode.splitValue = {'Cell attached'};
+analysisPreset.chan2Mode.splitValue = {'Cell attached'};
+analysisPreset.chan3Mode.splitValue = {'Cell attached'};
+analysisPreset.chan4Mode.splitValue = {'Cell attached'};
 
+
+%% Build Analysis structure
+
+buildAnalysis('Example-LightStep-Analysis', analysisPreset)
+
+%% Get the finder for tree 
+
+finder = getFeatureFinder('Example-LightStep-Analysis', 'cellData', 'dc1');
+finder.getStructure().tostring()
 
 %%
