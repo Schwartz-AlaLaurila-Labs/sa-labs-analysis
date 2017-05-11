4931ba552100200385b27faa888a67965a53a2fe
Sathish Kumar Narayanan <ragav.sathish@gmail.com>
2017-05-11 12:15:31 +0300

    add and extract spikes

---
+++ src/main/matlab/+sa_labs/+extractors/extractSpikes.m
@@ -0,0 +1,21 @@
+function extractSpikes(analysis, group, varargin)
+
+ip = inputParser;
+ip.addParameter('device', '', @ischar);
+ip.parse(varargin{:});
+
+if strcmpi(group.splitParameter, 'devices')
+    device = group.splitValue;
+else
+    device = ip.Results.device;
+end
+
+id = strcat('SPIKES_', upper(device));
+
+for epoch = analysis.getEpochs(group)
+    data = epoch.get(id);
+    featureGroup.createFeature(id, data);
+end
+end
+
+
--- src/main/matlab/+sa_labs/+pre_processors/addSpikesToEpoch.m
+++ src/main/matlab/+sa_labs/+pre_processors/addSpikesToEpoch.m
@@ -11,26 +11,14 @@ checkDetection = ip.Results.checkDetection;
 for epoch = cellData.epochs
     for device = each(devices)
         try
-            
             data = epoch.getResponse(device).quantity;
             spikeTime =  mht.spike_util.detectSpikes(data, 'checkDetection', checkDetection);
-            
             id = strcat('SPIKES', upper(device));
-            epoch.attributes(id) = createFeature(id, spikeTime);
+            epoch.add(id, spikeTime);
         catch
             disp(exception.message);
         end
     end
 end
 end
-
-function f = createFeature(id, data)
-import sa_labs.analysis.*;
-
-map = containers.Map();
-map('id') = id;
-description = entity.FeatureDescription(map);
-f = entity.Feature(description, data);
-end
-
 
